e611c40ed6a1fedb03af60c74cfd991b0248c799
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-12-21 01:45:48 +0300

    refactor(tenancy): add tenancy label keys

--- internal/shared.go
+++ internal/shared.go
@@ -1,7 +1,15 @@
 package internal
 
-// Fleet resource postfixes
+// Tenancy labels
+const (
+	ORGANIZATION_LABEL_KEY      = "robolaunch.io/organization"
+	TEAM_LABEL_KEY              = "robolaunch.io/team"
+	REGION_LABEL_KEY            = "robolaunch.io/region"
+	CLOUD_INSTANCE_LABEL_KEY    = "robolaunch.io/cloud-instance"
+	PHYSICAL_INSTANCE_LABEL_KEY = "robolaunch.io/physical-instance"
+)
 
+// Fleet resource postfixes
 const (
 	DISCOVERY_SERVER_FLEET_POSTFIX = "-discovery"
 )

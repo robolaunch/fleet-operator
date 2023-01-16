71278b8ff0d1c0c45329c4a5585c1ae36107d608
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-01-16 03:29:54 +0300

    refactor(namespace): add function to create federated namespace

--- pkg/controllers/fleet/create.go
+++ pkg/controllers/fleet/create.go
@@ -6,6 +6,9 @@ import (
 	"github.com/robolaunch/fleet-operator/internal/resources"
 	fleetv1alpha1 "github.com/robolaunch/fleet-operator/pkg/api/roboscale.io/v1alpha1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
@@ -27,6 +30,55 @@ func (r *FleetReconciler) createNamespace(ctx context.Context, instance *fleetv1
 	}
 
 	logger.Info("STATUS: Namespace " + ns.Name + " is created.")
+	return nil
+}
+
+func (r *FleetReconciler) createFederatedNamespace(ctx context.Context, instance *fleetv1alpha1.Fleet, nsNamespacedName *types.NamespacedName) error {
+
+	resourceInterface := r.DynamicClient.Resource(schema.GroupVersionResource{
+		Group:    "types.kubefed.io",
+		Version:  "v1beta1",
+		Resource: "federatednamespaces",
+	})
+
+	instancesMapSlice := []map[string]interface{}{}
+	for _, i := range instance.Spec.Instances {
+		instancesMapSlice = append(instancesMapSlice, map[string]interface{}{
+			"name": i,
+		})
+	}
+
+	federatedNamespace := unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "types.kubefed.io/v1beta1",
+			"kind":       "FederatedNamespace",
+			"metadata": map[string]interface{}{
+				"name":      nsNamespacedName.Name,
+				"namespace": nsNamespacedName.Name,
+				"ownerReferences": []map[string]interface{}{
+					{
+						"apiVersion":         instance.APIVersion,
+						"blockOwnerDeletion": true,
+						"controller":         true,
+						"kind":               instance.Kind,
+						"name":               instance.Name,
+						"uid":                instance.UID,
+					},
+				},
+			},
+			"spec": map[string]interface{}{
+				"placement": map[string]interface{}{
+					"clusters": instancesMapSlice,
+				},
+			},
+		},
+	}
+
+	_, err := resourceInterface.Namespace(nsNamespacedName.Name).Create(ctx, &federatedNamespace, v1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
